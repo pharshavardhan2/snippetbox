23bfcd4a685f67c5df38e26978d6a9a038bcb90e
Harsha <pharshavardhan2@outlook.com>
2024-04-01 18:15:45 +0530

    separated routes from main

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -28,22 +28,11 @@ func main() {
 		errorLog: errorLog,
 	}
 
-	// mux acts as a router mapping url patterns to their handlers
-	mux := http.NewServeMux()
-
-	// fileServer is used for serving static(files) over http
-	fileServer := http.FileServer(http.Dir("./ui/static/"))
-
-	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
-	mux.HandleFunc("/", app.home)
-	mux.HandleFunc("/snippet/view", app.snippetView)
-	mux.HandleFunc("/snippet/create", app.snippetCreate)
-
 	// http.Server is used for configuring server
 	server := &http.Server{
 		Addr:     *addr,
 		ErrorLog: errorLog,
-		Handler:  mux,
+		Handler:  app.routes(),
 	}
 
 	infoLog.Printf("Starting server on %s", *addr)
